4a7bf884643828fc2431714197abb20d2fd57257
otodreas <159846818+otodreas@users.noreply.github.com>
2025-12-13 16:31:45 +0100

    analysis cont

--- scripts/Analysis.R
+++ scripts/Analysis.R
@@ -1,9 +1,13 @@
 # Clear variables
 rm(list = ls())
 
-# Mount libraries
+# Reset plotting device
+# dev.off()
+
+# Attach packages
 library(tidyverse)
 library(here)
+library(MASS)
 
 # Define function to read and process metadata
 read_metadata <- function(rawdata_df, mdata_path) {
@@ -18,5 +22,10 @@ read_metadata <- function(rawdata_df, mdata_path) {
 }
 
 # Read data and metadata
-d <- as_tibble(read.csv(here("data", "exam2023_data-2.csv")))
+d <- as_tibble(read.csv(here("data", "exam2023_data-2.csv"))) %>%
+  drop_na()
 mdata <- read_metadata(d, here("data", "exam2023_metadata-2.csv"))
+
+# Create mixed linear model
+var <- d$ExoticAnnualHerb_cover
+plot(var, d$Euc_canopy_cover, col = as.factor(d$Season))
