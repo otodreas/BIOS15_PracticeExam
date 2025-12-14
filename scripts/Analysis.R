c6d714ac7a36db6d22f1d4edfcd7e67a582b6e4e
otodreas <159846818+otodreas@users.noreply.github.com>
2025-12-14 21:47:54 +0100

    comments

--- scripts/Analysis.R
+++ scripts/Analysis.R
@@ -2,6 +2,7 @@
 # Configure environment
 # =====================
 
+
 # Clear variables
 rm(list = ls())
 
@@ -14,9 +15,12 @@ library(rlang)
 library(tidyverse)
 
 
+
+
 # ================
 # Define functions
 # ================
+
 
 # Define function to read and process metadata
 read_metadata <- function(mdata_path, rawdata_df) {
@@ -64,33 +68,53 @@ predict_custom <- function(m1, m2, col_a, col_b, data) {
 
 # Get general summary statistics
 general_table <- function() {
+  # Create df to store stats in
   table <- data.frame(stats = c("mean", "sd"))
+  
+  # Create columns with mean and sd of each predictor and response
   table$annual <- c(mean(d$ExoticAnnualGrass_cover), sd(d$ExoticAnnualGrass_cover))
   table$perennial <- c(mean(d$ExoticPerennialGrass_cover), sd(d$ExoticPerennialGrass_cover))
   table$tall <- c(mean(d$tall), sd(d$tall))
   table$short <- c(mean(d$short), sd(d$short))
+
+  # Pipe table into mutate and apply significant figure rounding
   table <- table |>
     mutate(across(where(is.numeric), ~ signif(.x, digits = 3)))
+
+  # Write csv
   write.csv(table, here("outputs", "general_table.csv"))
 }
 
+# Create table with icc values to quantify %variance explained by random effects
 icc_table <- function(m1, m2, mname1, mname2) {
+  # Create a dataframe and store unadjusted iccs for each model in it
   table <- data.frame(unadj_icc = c(icc(m1)[, 2], icc(m2)[, 2]))
   table$model <- c(mname1, mname2)
+
+  # Write csv
   write.csv(table, here("outputs", "icc.csv"))
 }
 
+# Create table with distributions of random effect model intercepts
 group_dist <- function() {
+  # Get summary of all intercepts in short and tall model, excluding mean
   short <- summary(exp(coef(m_short)$cond$Property[,1]))[-4]
   tall <- summary(exp(coef(m_tall)$cond$Property[,1]))[-4]
+
+  # Insert each summary as a row in the dataframe table
   table <- data.frame(rbind(short, tall))
+
+  # Pipe table into mutate and apply significant figure rounding
   table <- table |>
     mutate(across(where(is.numeric), ~ signif(.x, digits = 3)))
+
+  # Write csv
   write.csv(table, here("outputs", "group_dist.csv"))
 }
 
-# Get % change in euc for each model range
+# Get % change in euc for each model range and write it into a text file
 get_pred_ranges <- function() {
+  # Get max and min for each prediction
   tamax <- preds$euc_tall_annual_pred[101]
   tamin <- preds$euc_tall_annual_pred[1]
   tpmax <- preds$euc_tall_perennial_pred[101]
@@ -100,23 +124,32 @@ get_pred_ranges <- function() {
   spmax <- preds$euc_short_perennial_pred[101]
   spmin <- preds$euc_short_perennial_pred[1]
 
+  # Create lists and vectors to loop across when writing the text file
   params <- list(c(tamin, tamax), c(tpmin, tpmax), c(samin, samax), c(spmin, spmax))
   labels <- c("tall_annual: ", "tall_peren: ", "short_annual: ", "short_peren: ")
+
+  # Produce the first line of the file containing an explanation of the file
   txt <- "Predicted change in response by predictor going from 0-100% for both models"
 
+  # Loop through each combination of predictor and response, updating the text file with ranges and % change
   for (i in seq_along(labels)) {
     txt[i+1] <- (paste0(
       labels[i], signif(params[[i]][1], 2), "-", signif(params[[i]][2], 2), ", ",
       signif((params[[i]][2] - params[[i]][1]) / params[[i]][1] * 100, 2), "%"
     ))
+
+  # Open, write, and close file
   writeLines(txt, here("outputs", "model_predictions.txt"))
   }
 }
 
 
-# ==============
-# Run operations
-# ==============
+
+
+# ===========
+# Run program
+# ===========
+
 
 # Read data and metadata
 d <- as_tibble(read.csv(here("data", "exam2023_data-2.csv"))) |>
@@ -147,10 +180,11 @@ m_short <- glmmTMB(
 # Generate predictions
 preds <- predict_custom(m_tall, m_short, ExoticPerennialGrass_cover, ExoticAnnualGrass_cover, d)
         
-# Create tables
+# Create tables and model reports
 general_table()
 icc_table(m_short, m_tall, "Number of eucalypt seedlings <50 cm/quadrat", "Number of eucalypt seedlings >50 cm/quadrat")
 group_dist()
+get_pred_ranges()
 
 # Plot tall plant frequency over exotic annual grass cover with best fit lines
 p_annual_tall <- ggplot(d) +
@@ -195,7 +229,3 @@ p_perennial_short <- ggplot(d) +
 # Create plot layout
 (p_annual_tall | p_perennial_tall) /
 (p_annual_short | p_perennial_short)
-
-# Write model prediction summary to txt
-# Get % change in euc for each model range
-get_pred_ranges()
